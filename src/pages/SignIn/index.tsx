7f0a2166c50281fd2c07d3028f9ea433ab94dfa1
Daniel Zordan <daniel.zordan@vtinova.com.br>
2020-09-08 11:56:26 -0400

    finishing signup and signin features

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,7 +3,7 @@ import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import { useAuth } from '../../hooks/auth';
 import { useToast } from '../../hooks/toast';
@@ -27,6 +27,7 @@ const SignIn: React.FC = () => {
 
   const { signIn } = useAuth();
   const { addToast } = useToast();
+  const history = useHistory();
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
@@ -48,6 +49,8 @@ const SignIn: React.FC = () => {
           password: data.password,
         });
 
+        history.push('/dashboard');
+
         addToast({
           type: 'success',
           title: 'Login efetuado com sucesso',
@@ -67,7 +70,7 @@ const SignIn: React.FC = () => {
         });
       }
     },
-    [signIn, addToast],
+    [signIn, addToast, history],
   );
 
   return (
